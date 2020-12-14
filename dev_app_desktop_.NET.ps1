d9327befbc9314879e28834a8e39d798040d02e7
Kori Francis <kfrancis@clinicalsupportsystems.com>
2020-12-14 12:42:25 -0500

    Adjust ubuntu and add lansweeper

--- dev_app_desktop_.NET.ps1
+++ dev_app_desktop_.NET.ps1
@@ -52,11 +52,13 @@ choco install -y microsoft-windows-terminal
 choco install -y putty.install
 choco install -y gitkraken
 choco install -y filezilla
-choco install -y sql-server-management-studio
+choco install -y ssms
+RefreshEnv
 choco install -y sqlsearch
 choco install -y screentogif
 choco install -y monosnap
 choco install -y sqltoolbelt
+choco install -y azure-data-studio
 
 choco install hackfont firacode inconsolata dejavufonts robotofonts droidfonts -y
 Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online -NoRestart
@@ -64,16 +66,9 @@ Enable-WindowsOptionalFeature -FeatureName Containers -Online -NoRestart
 Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -NoRestart
 
 #--- Ubuntu ---
-# TODO: Move this to choco install once --root is included in that package
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu -OutFile ~/Ubuntu.appx -UseBasicParsing
-Add-AppxPackage -Path ~/Ubuntu.appx
-# run the distro once and have it install locally with root user, unset password
+choco install wsl-ubuntu-2004 --params "/InstallRoot:true"
 
 RefreshEnv
-Ubuntu2004 install --root
-Ubuntu2004 run apt update
-Ubuntu2004 run apt upgrade -y
-
 choco install -y googlechrome
 choco install -y firefox
 choco install -y microsoft-edge-insider
@@ -91,6 +86,8 @@ choco install -y notepadplusplus
 choco install -y paint.net
 choco install -y nugetpackageexplorer
 choco install -y nuget.commandline
+choco install -y insomnia-rest-api-client
+choco install lsagent
 
 #--- reenabling critial items ---
 Enable-UAC
